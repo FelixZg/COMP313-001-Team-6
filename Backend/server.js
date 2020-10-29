f22a1e218c5b495c3053e80bb22550cce555bbe7
diaploblical <egeh2x@gmail.com>
2020-10-29 11:23:49 -0400

    user registration functionality completed

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+var { pool } = require('./dbConfig')
+var { body, validationResult } = require('express-validator')
+var session = require('express-session')
+var bcrypt = require('bcrypt')
+var passport = require('passport')
+
 const PORT = process.env.PORT || 3000
-const { pool } = require('./dbConfig')
-const { body, validationResult } = require('express-validator')
-const session = require('express-session')
+const INITIALIZE_PASSPORT = require('./passportConfig')
+
+INITIALIZE_PASSPORT(passport)
 
 app.use(express.urlencoded( { extended: false }))
-
-app.get('/', (req, res) => {
-  console.log("Got a GET request for the homepage")
-  res.send('Hello GET')
-})
+app.use(session( {
+  secret: 'secret', resave: false, saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.post('/signup', [
   body('email').isEmail().withMessage("Invalid email address"),
@@ -21,16 +27,51 @@ app.post('/signup', [
       return value
     }
   }),
-], (req, res) => {
+], async (req, res) => {
   var errors = validationResult(req).array()
   if (errors.length > 0) {
     console.log(errors)
   } else {
-    const email = req.body.email
-    const password = req.body.password
-    const confirmPassword = req.body.confirmPassword
-    console.log(email, password, confirmPassword)
+    let email = req.body.email
+    let password = req.body.password
+    let hashedPassword = await bcrypt.hash(password, 5)
+    
+    pool.query(
+      `SELECT * FROM users WHERE email = $1`, [email], (err, results) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log(results.rows)
+        }
+        if (results.rows.length > 0){
+          console.log("An account using this email address already exists")
+        } else {
+          pool.query(
+            `INSERT INTO users (email, password) 
+            VALUES ($1, $2)
+            RETURNING id, password`, [email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err
+              } else {
+                console.log(results.rows)
+              }           
+            }
+          )
+        }
+      }
+    )
   }
 })
+
+app.post('/login', passport.authenticate('local', (error, user, info) => {
+  if (error) {
+    console.log(error)
+  } else if (user) {
+    console.log(user)
+  } else {
+    console.log(info)
+  }
+}))
 
 app.listen(3000, () => console.log(`App listening on port ${PORT}!`))
